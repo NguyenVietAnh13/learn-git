288fa28043aedd7adf3e20daa30c3bd0abdd49d7
NguyenVietAnh13 <vanhnehss@gmail.com>
2024-12-11 15:12:25 +0700

    add user controller

---
+++ src/main/java/com/git/learn_git/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.git.learn_git.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/users")
+    public ResponseEntity<String> getAllUsers() {
+        return ResponseEntity.ok().body("All users.");
+    }
+}
